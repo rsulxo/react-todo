87c24893f9cf5224ba5327b6f5380e36163c00bb
rsulxo <rooha14_@hotmail.com>
2019-07-30 10:24:38 +0100

    a basic react to-do list app

---
+++ build/precache-manifest.a5c2ae91588442989a05494dc4ad19f4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b864b0d6c4205ec9d93b9582713c330",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c499550ea38ca698fe5f",
+    "url": "/static/css/main.ff06ebdb.chunk.css"
+  },
+  {
+    "revision": "8abba0e2e7fac975700b",
+    "url": "/static/js/2.e0534c4e.chunk.js"
+  },
+  {
+    "revision": "c499550ea38ca698fe5f",
+    "url": "/static/js/main.bc84fa7d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
